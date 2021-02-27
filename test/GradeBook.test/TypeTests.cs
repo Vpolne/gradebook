0a0a43ca5b5c7becb18e37d44224a5fd1bb08362
Stepan Kutyrev <SKKutyrev@outlook.com>
2021-02-27 23:29:58 +0300

    new test - TwoVarsCanReferenceSameObject

--- test/GradeBook.test/TypeTests.cs
+++ test/GradeBook.test/TypeTests.cs
@@ -6,7 +6,7 @@ namespace GradeBook.tests
     public class TypeTests
     {
         [Fact]
-        public void Test1()
+        public void GetBookReturnsDifferentObjects()
         {
             //arrange
             var book1 = GetBook("Book 1");
@@ -17,7 +17,18 @@ namespace GradeBook.tests
             Assert.Equal("Book 1",book1.Name);
             Assert.Equal("Book 2",book2.Name);
         }
-
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+            //act
+            book1 = book2;
+            //assert
+            Assert.Same(book1,book2);
+            Assert.True(Object.ReferenceEquals(book1,book2));
+        }
         Book GetBook(string name)
         {
             return new Book(name);
