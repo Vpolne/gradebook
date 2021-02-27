ae32f2a9c64bd0665c6d51e409e3563ba4bd1ae7
Vpolne <SKKutyrev@outlook.com>
2021-02-27 21:26:28 +0300

    Initial load

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book (string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            
+            foreach (var grade in grades)
+            {
+                result.Average+=grade;
+                result.High = Math.Max(grade,result.High);
+                result.Low = Math.Min(grade,result.Low);   
+            }
+            result.Average/=grades.Count;
+            return result;
+        }
+        public void ShowStatistics()
+        {
+            var result = GetStatistics();
+            Console.WriteLine($"Average grade is {result.Average:N1}");
+            Console.WriteLine($"The highest grade is {result.High:N1}");
+            Console.WriteLine($"The lowest grade is {result.Low:N1}");
+        }
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Vpolne Grade Book");
+            book.AddGrade(85.5);
+            book.AddGrade(90.2);
+            book.AddGrade(99.6);
+            book.ShowStatistics();
+        }
+    }
+}
