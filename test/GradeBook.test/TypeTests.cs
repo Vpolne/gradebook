c0c86997c79f1bb0eeba32fc6a6dff0eab35b089
Stepan Kutyrev <SKKutyrev@outlook.com>
2021-02-28 08:48:00 +0300

    new

--- test/GradeBook.test/TypeTests.cs
+++ test/GradeBook.test/TypeTests.cs
@@ -6,6 +6,70 @@ namespace GradeBook.tests
     public class TypeTests
     {
         [Fact]
+        public void ValueTypeAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+            Assert.Equal(42,x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z=42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            //act
+            GetBookSetNameByRef(ref book1,"New Name");
+            //assert
+            Assert.Equal("New Name",book1.Name);
+        }
+
+        private void GetBookSetNameByRef(ref Book book, string newname)
+        {
+            book = new Book(newname);
+        }
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            //act
+            GetBookSetName(book1,"New Name");
+            //assert
+            Assert.Equal("Book 1",book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string newname)
+        {
+            book = new Book(newname);
+        }
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            //act
+            SetName(book1,"New Name");
+            //assert
+            Assert.Equal("New Name",book1.Name);
+        }
+
+        private void SetName(Book book1, string newname)
+        {
+            book1.Name = newname;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             //arrange
