346798064772da93786f2dfc40dc1e420a265be9
Stepan Kutyrev <SKKutyrev@outlook.com>
2021-03-02 23:31:06 +0300

    new method - AddLetterGrade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -23,6 +23,27 @@ namespace GradeBook
             }
             
         }
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;                
+            }
+        }
 
         public Statistics GetStatistics()
         {
@@ -34,10 +55,34 @@ namespace GradeBook
             foreach (var grade in grades)
             {
                 result.Average+=grade;
-                result.High = Math.Max(grade,result.High);
                 result.Low = Math.Min(grade,result.Low);   
+                result.High = Math.Max(grade,result.High);
             }
+
             result.Average/=grades.Count;
+            
+            switch(result.Average)
+            {
+                case var d when d>=90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d>=80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d>=70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d>=60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
             return result;
         }
         public void ShowStatistics()
@@ -46,6 +91,7 @@ namespace GradeBook
             Console.WriteLine($"Average grade is {result.Average:N1}");
             Console.WriteLine($"The highest grade is {result.High:N1}");
             Console.WriteLine($"The lowest grade is {result.Low:N1}");
+            Console.WriteLine($"The letter is {result.Letter}");
         }
         public List<double> GetGrades()
         {
--- test/GradeBook.test/BookTests.cs
+++ test/GradeBook.test/BookTests.cs
@@ -21,6 +21,7 @@ namespace GradeBook.tests
             Assert.Equal(85.6,result.Average,1);
             Assert.Equal(90.5,result.High,1);
             Assert.Equal(77.3,result.Low,1);
+            Assert.Equal('B',result.Letter);
         }
         [Fact]
         public void GradesAreInCorrectRange()
